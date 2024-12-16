745506fd97c467b62e4e3787e0b2597aa21244d8
Diego Maldonado <dmnunez1993@gmail.com>
2024-12-15 21:06:27 -0300

    fixed errors in prod layout

--- prod/nginx/entrypoint.sh
+++ prod/nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '${NGINX_BACKEND_PROXY} ${NGINX_BACKEND_HOST} ${NGINX_END_USER_PROXY} ${NGINX_END_USER_APP_HOST}' < /nginx.conf.template > /etc/nginx/conf.d/app.conf
+envsubst '${NGINX_BACKEND_PROXY} ${NGINX_BACKEND_HOST} ${NGINX_END_USER_APP_PROXY} ${NGINX_END_USER_APP_HOST}' < /nginx.conf.template > /etc/nginx/conf.d/app.conf
 
 exec "$@"
